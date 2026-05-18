ea9a0f36df118faccbbe67a86f687e8675ec9469
Abdul Qadeer Khan <iaqkhan222@gmail.com>
2026-05-18 16:19:03 +0500

    updated frontend api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' }
+  baseURL: "http://localhost:3001/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export const getTasks = () => api.get('/tasks');
-export const createTask = (task) => api.post('/tasks', task);
-export const updateTask = (id, task) => api.put(`/tasks/${id}`, task);
-export const deleteTask = (id) => api.delete(`/tasks/${id}`);
+export const getTasks = () => api.get("/tasks");
+
+export const createTask = (task) => api.post("/tasks", task);
+
+export const updateTask = (id, task) =>
+  api.put(`/tasks/${id}`, task);
+
+export const deleteTask = (id) =>
+  api.delete(`/tasks/${id}`);
